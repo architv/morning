14fa1c5966f4d081b909e343353f329de85b2a46
sauravtom <stomatrix@gmail.com>
2015-03-31 02:30:44 +0530

    unable to populate angular feed

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
 
-var feed= new Array();
-
 angular.module('morningapp', ['ionic'])
 
 .controller('FeedController', function($scope, $http) {
 
   window.onload = function(e){ 
       Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1dBRTBwNMzbCBriwe1evK4mxfo12r-q5ZRiUFBuP1RQE/pubhtml?gid=0&single=true',
-                       callback: showInfo} ); 
+                       callback: showInfo,
+                     simpleSheet: true} ); 
   }
   
 
   function showInfo(data,tabletop) { 
-    feed_ = data['main']['elements'];
-    console.log(feed_);
-    //$scope.feed = feed_; 
+    console.log(data);
+    $scope.feed = data;
+    window.data = data;
   }
 
-  feed2 = [
+  test_feed = [
      { title: 'Collect coins' },
      { title: 'Eat mushrooms' },
      { title: 'Get high enough to grab the flag' },
      { title: 'Find the Princess' }
    ];
    
-  $scope.feed = feed2;
-   
+  $scope.feed = window.data;
+  //$scope.feed = test_feed;
+
   
 });
