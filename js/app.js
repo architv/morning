e3f91d1b13649a1a5c66ad84dd9310863612a483
sauravtom <stomatrix@gmail.com>
2015-03-31 03:02:32 +0530

    more UI changes

--- js/app.js
+++ js/app.js
@@ -3,28 +3,11 @@ angular.module('morningapp', ['ionic'])
 
 .controller('FeedController', function($scope, $http) {
 
-  window.onload = function(e){ 
-      Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1dBRTBwNMzbCBriwe1evK4mxfo12r-q5ZRiUFBuP1RQE/pubhtml?gid=0&single=true',
-                       callback: showInfo,
-                     simpleSheet: true} ); 
-  }
-  
-
-  function showInfo(data,tabletop) { 
-    console.log(data);
-    $scope.feed = data;
-    window.data = data;
-  }
-
-  test_feed = [
-     { title: 'Collect coins' },
-     { title: 'Eat mushrooms' },
-     { title: 'Get high enough to grab the flag' },
-     { title: 'Find the Princess' }
-   ];
-   
-  $scope.feed = window.data;
-  //$scope.feed = test_feed;
-
+  $http.get('http://orch.in/morningapp').then(function(resp) {
+      console.log('Success', resp.data.data);
+      $scope.feed = resp.data.data;
+    }, function(err) {
+      console.error('ERR', err);
+    })
   
 });
