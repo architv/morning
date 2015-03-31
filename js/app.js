794ba65b3ffe8400626876296e2ff3552aeece59
Archit Verma <architv07@gmail.com>
2015-03-31 08:21:09 +0530

    added loader

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 
 angular.module('morningapp', ['ionic'])
 
-.controller('FeedController', function($scope, $http) {
+.controller('FeedController', function($scope, $http, $ionicLoading) {
+
+  $ionicLoading.show({
+    content: 'Loading',
+    animation: 'fade-in',
+    showBackdrop: true,
+    maxWidth: 200,
+    showDelay: 0
+  });
 
   $http.get('http://orch.in/morningapp').then(function(resp) {
       console.log('Success', resp.data.data);
+      $ionicLoading.hide();
       $scope.feed = resp.data.data;
     }, function(err) {
       console.error('ERR', err);
