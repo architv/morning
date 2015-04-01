01f52f30da53e20014ecb42e531ac29ec890d135
Archit Verma <architv07@gmail.com>
2015-04-02 01:06:32 +0530

    changed icon for favourite and share

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
-angular.module('morningapp', ['ionic'])
+angular.module('morningapp', ['ionic', 'ngCordova'])
 
-.controller('FeedController', function($scope, $http, $ionicLoading) {
+.controller('FeedController', function($scope, $http, $ionicLoading, $cordovaSocialSharing) {
 
   $ionicLoading.show({
     content: 'Loading',
@@ -18,5 +18,17 @@ angular.module('morningapp', ['ionic'])
     }, function(err) {
       console.error('ERR', err);
     })
+
+  $scope.shareAnywhere = function() {
+    $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "http://blog.nraboy.com");
+  }
+ 
+  $scope.shareViaTwitter = function(message, image, link) {
+    $cordovaSocialSharing.canShareVia("twitter", message, image, link).then(function(result) {
+        $cordovaSocialSharing.shareViaTwitter(message, image, link);
+    }, function(error) {
+        alert("Cannot share on Twitter");
+    });
+  }
   
 });
